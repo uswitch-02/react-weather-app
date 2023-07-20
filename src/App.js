43142ae33eb4b9f126966ff72a898106794c098a
uswitch-02 <chunxidubian64@gmail.com>
2023-07-20 12:51:06 +0000

    [start]title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import Title from "./components/Title";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>こんにちは</h1>
+    <div className="test">
+      <Title />
     </div>
   );
 }
