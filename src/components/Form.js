0e2f6d6c78194323d3533c1b63473f3a4d361d74
uswitch-02 <chunxidubian64@gmail.com>
2023-07-22 10:40:22 +0000

    [Error Fixing]manifest.json-console-error

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,21 @@
 import { useState } from "react" ;
+import axios from "axios";
+
+
 
 const Form = () => {
-  const [city, setCity] = useState("");
-  return (
-        <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-            {city}
-          <button type="submit">Get Weather</button>
-        </form>
-    );
-};
+    const [city, setCity] = useState("");
+    const getWeather = () => {
+        axios.get("https://api.weatherapi.com/v1/current.json?key=210b8e4818a4439694091142232207&q=London&aqi=no")
+          .then(res => console.log(res))
+    }
+    return (
+          <form>
+              <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
+
+                  <button type="submit" onClick={getWeather}>Get Weather</button>
+          </form>
+      );
+  };
 
 export default Form;
